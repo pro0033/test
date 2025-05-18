ce15357f9c2ec3b51c76395ac7175c5389db4893
pro0033 <123844931+pro0033@users.noreply.github.com>
2025-05-18 21:42:00 +0200

    Add files via upload

---
+++ types.tsx
@@ -0,0 +1,14 @@
+export interface Product {
+  id: number
+  name: string
+  description: string
+  price: number
+  image: string
+  category: string
+  rating: number
+  reviews: number
+  brand: string
+  material: string
+  weight: string
+  dimensions: string
+}
